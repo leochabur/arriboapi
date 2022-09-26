388082b2696e0d7612294eb44ebfdf2521452bf3
unknown <Leo@LEO-PC.DOMAIN.local>
2022-09-26 11:05:16 -0300

    pdpap

--- v1/rest.php
+++ v1/rest.php
@@ -1,19 +1,11 @@
 <?php
-/*header('Access-Control-Allow-Origin: http://localhost:3000');
-header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
-header('Content-Type: application/json');*/
-// Specify domains from which requests are allowed
+
 header('Access-Control-Allow-Origin: *');
 
-// Specify which request methods are allowed
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
-// Additional headers which may be sent along with the CORS request
-// The X-Requested-With header allows jQuery requests to go through
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
-// Set the age to 1 day to improve speed/caching.
 header('Access-Control-Max-Age: 86400');
 
 require_once ('../lib/nusoap.php');
