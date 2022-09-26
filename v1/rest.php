70bbc8fa7fc497d3b30ba1a388d5b0903a89e0a5
unknown <Leo@LEO-PC.DOMAIN.local>
2022-09-26 09:01:55 -0300

    update

--- v1/rest.php
+++ v1/rest.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Expose-Headers: Content-Length, X-JSON");
+header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: *");
 
 //error_reporting(E_ALL);
 /**
@@ -21,9 +25,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 header('Content-Type: text/html; charset=utf-8');
 header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"'); */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 //header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 //header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
