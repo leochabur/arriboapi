15d9816b708588cf044f6f275d55f9cb51837ca6
= <leochabur@gmail.com>
2022-09-25 21:43:37 -0300

    initial commit

--- v1/rest.php
+++ v1/rest.php
@@ -21,6 +21,10 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 header('Content-Type: text/html; charset=utf-8');
 header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"'); */
 
+//header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 
 
 require_once ('../lib/nusoap.php');
