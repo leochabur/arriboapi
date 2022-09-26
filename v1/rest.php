2e74963723f0632fe9b6b99114d9002c0b1a054b
unknown <Leo@LEO-PC.DOMAIN.local>
2022-09-26 10:59:44 -0300

    pdpap

--- v1/rest.php
+++ v1/rest.php
@@ -1,47 +1,31 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+/*header('Access-Control-Allow-Origin: http://localhost:3000');
 header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
-header('Content-Type: application/json');
+header('Content-Type: application/json');*/
+// Specify domains from which requests are allowed
+header('Access-Control-Allow-Origin: *');
 
-//error_reporting(E_ALL);
-/**
- *
- * @About:      API Interface
- * @File:       index.php
- * @Date:       $Date:$ Nov-2015
- * @Version:    $Rev:$ 1.0
- * @Developer:  Federico Guzman (federicoguzman@gmail.com)
- **/
+// Specify which request methods are allowed
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
-/* Los headers permiten acceso desde otro dominio (CORS) a nuestro REST API o desde un cliente remoto via HTTP
- * Removiendo las lineas header() limitamos el acceso a nuestro RESTfull API a el mismo dominio
- * Nótese los métodos permitidos en Access-Control-Allow-Methods. Esto nos permite limitar los métodos de consulta a nuestro RESTfull API
- * Mas información: https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
- **/
-/*header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
-header("Access-Control-Allow-Headers: X-Requested-With");
-header('Content-Type: text/html; charset=utf-8');
-header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"'); */
+// Additional headers which may be sent along with the CORS request
+// The X-Requested-With header allows jQuery requests to go through
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
-
-//header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-//header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
-
+// Set the age to 1 day to improve speed/caching.
+header('Access-Control-Max-Age: 86400');
 
 require_once ('../lib/nusoap.php');
-
-
-/* Puedes utilizar este file para conectar con base de datos incluido en este demo; 
- * si lo usas debes eliminar el include_once del file Config ya que le mismo está incluido en DBHandler 
- **/
-//require_once '../include/DbHandler.php'; 
 
 require '../libs/Slim/Slim.php'; 
 \Slim\Slim::registerAutoloader(); 
 $app = new \Slim\Slim();
+
+if($app->request->isOptions()) {
+   return true;
+   break;
+}
 
  function distanceGPS($lat1, $lon1, $lat2, $lon2, $unit) {
   
@@ -133,6 +117,7 @@ function procesarParadas($gpx, $pos)
 
 
 $app->post('/predictivo/calcular/:orden', function ($orden) use ($app) {
+
 
         function prueba($data){
             return $data;
@@ -336,6 +321,10 @@ $app->get('/gpx/:cron', function ($cron) use ($app) {
 });
 
 $app->get('/servicios/proximos', function () use ($app) {
+/*header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+header('Content-Type: application/json');*/
     try
     {
         $conn = mysqli_connect('mariadb-masterbus-trafico.planisys.net', 'c0mbexpuser', 'Mb2013Exp', 'c0mbexport');
@@ -689,6 +678,9 @@ function echoResponse($status_code, $response) {
  
     // setting response content type to json
     $app->contentType('application/json');
+    //$app->response->headers->set("Access-Control-Allow-Origin", "http://localhost:3000");
+    //$app->response->headers->set("Access-Control-Allow-Methods", "HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    //$app->header("Access-Control-Allow-Origin", "*");
  
     echo json_encode($response);
 }
