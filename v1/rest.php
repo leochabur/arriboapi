4c3d9a2b05345b4d47b0c0036dffddb4ef774e15
unknown <Leo@LEO-PC.DOMAIN.local>
2022-09-26 09:07:23 -0300

    update

--- v1/rest.php
+++ v1/rest.php
@@ -1,8 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Expose-Headers: Content-Length, X-JSON");
-header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+header('Content-Type: application/json');
 
 //error_reporting(E_ALL);
 /**
